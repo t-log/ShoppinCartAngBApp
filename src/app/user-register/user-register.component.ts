ed7fe7e572de77ea1cb817117eb7a647b9d530ec
akhil <akhil.paul@ymail.com>
2022-12-09 14:18:43 +0530

    Adds user validation and local storage

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-register',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent {
+  name=""
+  address=""
+  phoneNo=""
+  email=""
+  password=""
+  confirmPassword=""
 
+  constructor(private api:ApiService,private router:Router){}
+  readValues=()=>
+  {
+    let data:any={"name":this.name,"address":this.address,"phoneNo":this.phoneNo,
+    "email":this.email,"password":this.password}
+    console.log(data);
+    
+    this.api.addUser(data).subscribe(
+      (response:any)=>{
+        console.log(response);
+        if(response.status=="success"){
+          alert("User Added Successfully")}
+          
+        else{
+          alert("Something wrong happened!")
+        }
+        
+      }
+    )
+     
+  }
 }
